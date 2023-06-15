bba1ae8fcf43fa51c6f57271ec01aaf66d2e4fab
Junsung Park / Daanta <daanta@naver.com>
2023-06-15 10:29:26 +0900

    230615_1029 Project Initialize

---
+++ src/main/java/com/daanta/DaisoMuseum/DaisoMuseumApplication.java
@@ -0,0 +1,13 @@
+package com.daanta.DaisoMuseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisoMuseumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisoMuseumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daanta/DaisoMuseum/DaisoMuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daanta.DaisoMuseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaisoMuseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
